f6cf607e7ace2f9e92c70ad96898c6415c53b7ee
elDin-Dev <alexsmaildese-dev@yahoo.com>
2020-06-05 00:58:48 +0200

    basic react app

---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PizzaImage from '../components/PizzaImage/PizzaImage';
+
+export class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>yet another react app</p>
+            </div>
+        )
+    }
+}
+
+export default Users;
