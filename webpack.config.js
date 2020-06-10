1c7423e48baeaaba3c9d5336c1262cc67400cefc
elDin-Dev <alexsmaildese-dev@yahoo.com>
2020-06-11 01:22:49 +0200

    webpack config continue

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,11 @@ module.exports= {
         publicPath: ''
     },
     devtool: 'cheap-module-eval-source-map',
+    module: {
+        rules: {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+        }
+    }
 };
